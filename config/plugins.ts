b3778ea4c9e5df7784d2dbcfbc486820e889b38b
Andrii Maiskyi <andrey@wearehelful.com>
2025-07-18 15:17:31 +0100

    feat: gcp storage. #3

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,15 @@
-export default () => ({});
+export default ({ env }) => {
+  return {
+    upload: {
+      config: {
+        provider:
+          "@strapi-community/strapi-provider-upload-google-cloud-storage",
+        providerOptions: {
+          bucketName: env("GCP_STORAGE_BUCKET_NAME"),
+          publicFiles: true,
+          serviceAccount: env.json("GCP_STORAGE_SERVICE_ACCOUNT"),
+        },
+      },
+    },
+  };
+};
