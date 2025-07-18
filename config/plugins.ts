20a87783daf1e7908fe1c42528ef8288e8de3bdd
Andrii Maiskyi <andrey@wearehelful.com>
2025-07-18 16:47:29 +0100

    fix: build. #3

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,20 +1,17 @@
-export default ({ env }) => {
-  return {
-    upload: {
-      config: {
-        provider:
-          "@strapi-community/strapi-provider-upload-google-cloud-storage",
-        providerOptions: {
-          bucketName: env("GCP_STORAGE_BUCKET_NAME"),
-          publicFiles: true,
-          serviceAccount: JSON.parse(
-            Buffer.from(
-              env("GCP_STORAGE_SERVICE_ACCOUNT_BASE64"),
-              "base64"
-            ).toString("utf-8")
-          ),
-        },
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: "@strapi-community/strapi-provider-upload-google-cloud-storage",
+      providerOptions: {
+        bucketName: env("GCP_STORAGE_BUCKET_NAME"),
+        publicFiles: true,
+        serviceAccount: JSON.parse(
+          Buffer.from(
+            env("GCP_STORAGE_SERVICE_ACCOUNT_BASE64"),
+            "base64"
+          ).toString("utf-8")
+        ),
       },
     },
-  };
-};
+  },
+});
