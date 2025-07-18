1b2c931990fae5201672dc7edae98ac4dcc5d805
Andrii Maiskyi <andrey@wearehelful.com>
2025-07-18 17:03:48 +0100

    fix: build. #3

--- config/plugins.ts
+++ config/plugins.ts
@@ -5,15 +5,15 @@ export default ({ env }) => ({
       providerOptions: {
         bucketName: env("GCP_STORAGE_BUCKET_NAME"),
         publicFiles: true,
-        serviceAccount: env("GCP_SERVICE_ACCOUNT_BASE64")
-          ? JSON.parse(
+        serviceAccount: (() => {
+          if (env("GCP_SERVICE_ACCOUNT_BASE64")) {
+            return JSON.parse(
               Buffer.from(env("GCP_SERVICE_ACCOUNT_BASE64"), "base64").toString(
                 "utf8"
               )
-            )
-          : (() => {
-              throw new Error("GCP_SERVICE_ACCOUNT_BASE64 is not defined");
-            })(),
+            );
+          }
+        })(),
       },
     },
   },
